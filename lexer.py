3e5c98e9b5790b0f9bb6c48c36fe914a0321c9a5
rajp4 <rajp4445@gmail.com>
2019-08-26 18:08:41 -0400

    Read file

--- lexer.py
+++ lexer.py
@@ -1,1 +1,9 @@
 # This project file will read the input and would return the token information tp parser file.
+
+def lexer(filename):
+    inputFile = open(filename, 'r')
+    for line in inputFile:
+        print((line.strip()))
+
+
+lexer('test')
