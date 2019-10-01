c0dd272d4f505ad5543f33cb69e1accf904cac2a
rajp4 <rajp4445@gmail.com>
2019-09-30 21:15:23 -0400

    Fix grammar, def states methods

--- parser.py
+++ parser.py
@@ -2,7 +2,7 @@
 import lexer
 
 tokenList = lexer.tokenList
-tokenList.append("$")  # todo verify if this needs to be done or not
+tokenList.append("$ $")  # todo verify if this needs to be done or not
 tokenIndex = -1
 currToken = ''
 
@@ -11,9 +11,110 @@ def nextToken():
     global tokenIndex, currToken
     tokenIndex = tokenIndex + 1
     try:
-        currToken = tokenList[tokenIndex]
+        tokenString = tokenList[tokenIndex]
+        currToken = tokenString.split(" ")
     except IndexError:
-        print("ERROR: end of token list")  # Should not call the method. The grammar should be accepted on the last "$"
+        currToken[0] = '$'
+        currToken[1] = '$'  # Should not call the method. The grammar should be accepted on the last "$"
 
-# def program():
-#     declarationList()
+
+def previousToken():
+    global tokenIndex, currToken
+    tokenIndex = tokenIndex - 1
+    try:
+        tokenString = tokenList[tokenIndex]
+        currToken = tokenString.split(" ")
+    except IndexError:
+        tokenString = tokenList[0]
+        currToken = tokenString.split(" ")  # Should not call the method. The grammar should be accepted on the last "$"
+
+
+def start():
+    if program():
+        nextToken()
+        if currToken[1] == '$':
+            print("ACCEPT")
+            return
+    print("REJECT")
+
+
+def program():
+    return declarationList()
+
+
+def declarationList():
+    nextToken()  # first token
+    if currToken[0] == "Keyword":
+        if (currToken[1] == "int" or currToken[1] == "void"):
+            if (declaration()):
+                if (fixedDecList()):
+                    return True
+    return False
+
+
+def fixedDecList():
+    nextToken()
+    if currToken[0] == "Keyword":
+        if (currToken[1] == "int" or currToken[1] == "void"):
+            if (declaration()):
+                fixedDecList()
+                return True
+    elif currToken[0] == "$":
+        return True
+    return False
+    # empty state todo DO I need to look into follow sets?
+
+
+def declaration():
+    nextToken()
+    if (currToken[0] == 'ID'):
+        nextToken()
+        if (currToken[0] == '('):  # goes to params() if ( exist
+            if params():
+                nextToken()
+                if (currToken[0] == ')'):
+                    compoundStmt()
+                    return True
+    return False
+
+
+def varDeclaration():
+    nextToken()
+    if currToken[0] == "Keyword":
+        if currToken[1] == "int" or currToken[1] == "void":
+            nextToken()
+            if currToken[0] == "ID":
+                nextToken()
+                if currToken[0] == "[":
+                    nextToken()
+                    if currToken[0] == "NUM":
+                        nextToken()
+                        if currToken[0] == "]":
+                            nextToken()
+                            if currToken[0] == ";":
+                                return True
+                elif currToken[0] == ";":
+                    return True
+    return False
+
+
+def params():
+    nextToken()
+    if currToken[0] == "Keyword":
+        if currToken[1] == "void":
+            nextToken()
+            if currToken[0] == "ID":
+                nextToken()
+                if currToken[0] == "[":
+                    if currToken[0] == "]":
+                        return True
+                elif currToken[0] == ")" or currToken[1] == ",":
+                    previousToken()
+                    fixedParList()
+            elif currToken[0] == ")":
+                previousToken()
+                return True
+    return False
+
+
+start()
